5f42fc1b515057c34fe7317c4ee6dbfb76a3468c
Nursultan <turdubaevnursultan@gmail.com>
2022-02-21 21:08:15 +0600

    fix/BugFix T1320

---
+++ src/main/java/com/digital_nomads/javaFall2021/applications/Amazon/AmazonTest.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.javaFall2021.applications.Amazon;
+
+public class AmazonTest {
+    public static void main(String[] args) {
+        System.out.println("This is Amazon Test");
+    }
+}
