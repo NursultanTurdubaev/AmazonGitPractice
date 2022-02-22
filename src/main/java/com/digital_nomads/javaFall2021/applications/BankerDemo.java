01c2fb03ccc2f45ac29c805751ff3c0a7d97dff7
Nursultan <turdubaevnursultan@gmail.com>
2022-02-22 20:07:11 +0600

    deleted souts

--- src/main/java/com/digital_nomads/javaFall2021/applications/BankerDemo.java
+++ src/main/java/com/digital_nomads/javaFall2021/applications/BankerDemo.java
@@ -8,17 +8,16 @@ public class BankerDemo {
         DemirBank nursultanDemirBankAccount = new DemirBank(123456789111L, 123456789);
         DemirBank urmatDemirBankAccount = new DemirBank(11800000222L, 987654321);
 
-        System.out.println(nursultanDemirBankAccount.getBalance());
+
         nursultanDemirBankAccount.deposit(5000);
         System.out.println("After deposit: "+ nursultanDemirBankAccount.getBalance());
 
         transferFunds(nursultanDemirBankAccount,urmatDemirBankAccount,2000);
 
-        System.out.println("Urmat's balance after transfer is: "+urmatDemirBankAccount.getBalance());
-        System.out.println("Nursultan's balance after transfer is: " +nursultanDemirBankAccount.getBalance());
+
 
         transferFunds(urmatDemirBankAccount,nursultanDemirBankAccount,100000);
-        System.out.println(nursultanDemirBankAccount.getBalance());
+
     }
 
 
