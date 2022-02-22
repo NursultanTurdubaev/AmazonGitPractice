e43c70ac1edf21ce4d7ff484cd1f47046f028c60
Nursultan <turdubaevnursultan@gmail.com>
2022-02-22 19:24:57 +0600

    added new java class

---
+++ src/main/java/com/digital_nomads/javaFall2021/applications/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.digital_nomads.javaFall2021.applications;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("this is test");
+
+    }
+}
